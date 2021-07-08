8374318e33fdce9bbb77636595ae4f63516ae858
@SoulOG <285963@supinfo.com>
2021-07-08 00:24:11 +0000

    Ajout du routage

---
+++ src/component/CurentlyReading.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class CurentlyReading extends Component{
+    render() {
+        return(
+            <div>Hello from curentlyReading</div>
+        )
+    }
+}
+
+export default CurentlyReading
