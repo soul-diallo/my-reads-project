6746f226ffe413637828e0f603d62240e909c778
@SoulOG <285963@supinfo.com>
2021-07-09 00:04:26 +0000

    Implementation de search book

--- src/component/SearchBooks.jsx
+++ src/component/SearchBooks.jsx
@@ -1,36 +1,83 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import * as BooksAPI from "../BooksAPI";
+import Book from "./books";
 
 class SearchBooks extends Component {
 
+    state = {
+        Books: [],
+        query: ''
+    }
 
+    static propTypes = {
+        onChange: PropTypes.func.isRequired,
+        myBooks: PropTypes.array.isRequired
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value;
+        this.setState(() => {
+            return {query: value}
+        })
+        this.search_books(value)
+    }
+
+    changeBookShelf = (books) => {
+        let all_Books = this.props.myBooks
+        for (let book of books) {
+            book.shelf = "none"
+        }
+
+        for (let book of books) {
+            for (let b of all_Books) {
+                if (b.id === book.id) {
+                    book.shelf = b.shelf
+                }
+            }
+        }
+        return books
+    }
+
+    search_books = (val) => {
+        if (val.length !== 0) {
+            BooksAPI.search(val, 10).then((books) => {
+                if (books.length > 0) {
+                    books = books.filter((book) => (book.imageLinks))
+                    books = this.changeBookShelf(books)
+                    this.setState(() => {
+                        return {Books: books}
+                    })
+                }
+            })
+        } else {
+            this.setState({Books: [], query: ''})
+        }
+    }
+
+    add_book = (book, shelf) => {
+        this.props.onChange(book, shelf)
+    }
 
     render() {
-
-
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link className="close-search" to="/">Close
-                    </Link>
+                    <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author"/>
-
+                        <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"/>
+                    <ol className="books-grid">
+                        {this.state.query.length > 0 && this.state.Books.map((book, index) => (<Book book={book} key={index} onUpdate={(shelf) => {
+                            this.add_book(book, shelf)
+                        }}/>))}
+                    </ol>
                 </div>
             </div>
-        );
+        )
     }
 }
 
